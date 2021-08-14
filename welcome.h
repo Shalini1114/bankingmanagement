ceb94eb7d8fb00681cf2e3e76550d5b981629298
MD Amzad <md.amzad.site.btech.cse.2@subhartiuniversit.onmicrosoft.com>
2021-08-14 18:59:58 +0530

    GUI changes and button sound added

--- welcome.h
+++ welcome.h
@@ -134,7 +134,7 @@ namespace bankingmanagement {
 			// timer1
 			// 
 			this->timer1->Enabled = true;
-			this->timer1->Interval = 15;
+			this->timer1->Interval = 1;
 			this->timer1->Tick += gcnew System::EventHandler(this, &welcome::timer1_Tick);
 			// 
 			// label3
