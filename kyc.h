af47144d7cdcdca3500b05df3ea34ff88915294f
MD Amzad <md.amzad.site.btech.cse.2@subhartiuniversit.onmicrosoft.com>
2021-08-31 00:05:08 +0530

    Databse changed

--- kyc.h
+++ kyc.h
@@ -295,7 +295,7 @@ private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e
 	
 	
 	// Connecting to database.
-	String^ ConnectString = "datasource=localhost;port=3306;username=abhishek;password=abhisha@11";
+	String^ ConnectString = "datasource=sql6.freemysqlhosting.net;port=3306;username=sql6433712;password=HLZ9vD1KIH";
 
 	MySqlConnection^ Connect = gcnew MySqlConnection(ConnectString);
 	String^ Query;
